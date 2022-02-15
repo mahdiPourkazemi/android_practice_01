7b5d58028073a7899f63db3e425f660cb75a9398
mahdi_dell <mahdi@pourkazemi.com>
2022-02-15 13:13:44 +0330

    first commit with very simple life cycle

---
+++ app/src/main/java/com/pourkazemi/mahdi/myfirstapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.pourkazemi.mahdi.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreateCalled")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause Called")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+}
